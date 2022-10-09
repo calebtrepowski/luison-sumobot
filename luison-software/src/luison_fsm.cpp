2afaedcded5925db160c758dfbd3ea8b89bd4860
Caleb Trepowski <calebtrepowski@gmail.com>
2022-10-09 20:59:13 -0300

    Nested FSM working for avoidFallFrontLeft

--- luison-software/src/luison_fsm.cpp
+++ luison-software/src/luison_fsm.cpp
@@ -7,18 +7,25 @@
 #include "onOffControl.h"
 #include "onOffInterruption.h"
 #include "proximity.h"
+// #include "fsm.h"
 
 typedef void (*STATE_HANDLER_T)();
+typedef void (*INNER_STATE_HANDLER_T)();
 
 void idle();
-void avoidFallFront();
+void avoidFallFrontLeft();
 void attackFront();
-void normalOperation();
+void normalSearch();
 void turnRight();
 void turnLeft();
 
-volatile STATE_HANDLER_T prior_state, state;
-uint_fast32_t avoidFallFrontTime, avoidFallFrontReverseTime;
+/* initial strategies */
+void diagonalAttack();
+void diagonalKickBack();
+
+volatile STATE_HANDLER_T priorState, state;
+volatile INNER_STATE_HANDLER_T priorInnerState, innerState;
+
 uint_fast8_t turnAngle;
 
 /* INTERRUPTION */
@@ -29,7 +36,7 @@ uint_fast32_t lastOnTime = 0;
 const uint_fast8_t debounceTime = 10;   // ms
 const uint_fast8_t minimumOnTime = 500; // ms
 
-uint_fast8_t strategy = 0;
+uint_fast8_t initialStrategy = 0;
 
 void IRAM_ATTR toggleIdleISR()
 {
@@ -42,8 +49,22 @@ void IRAM_ATTR toggleIdleISR()
 
             if (state == idle)
             {
-                strategy = dipSwitch::readInt();
-                state = normalOperation;
+                initialStrategy = dipSwitch::readInt();
+                switch (initialStrategy)
+                {
+                // case 1:
+                //     state = diagonalAttack;
+                //     break;
+                // case 2:
+                //     state = diagonalKickBack;
+                //     break;
+                // case 3:
+                // break;
+                default:
+                    state = normalSearch;
+                    break;
+                }
+
                 lastOnTime = buttonTime;
             }
             else
@@ -64,9 +85,7 @@ void setup()
     proximity::setup();
     attachInterrupt(onOffControl::control.pin, toggleIdleISR, RISING);
 
-    // prior_state = states::NONE;
-    // state = states::IDLE;
-    prior_state = NULL;
+    priorState = NULL;
     state = idle;
 #ifdef DEBUG
     Serial.begin(9600);
@@ -80,149 +99,231 @@ void loop()
 
 void idle()
 {
-    if (state != prior_state)
+    if (state != priorState)
     {
 #ifdef DEBUG
         Serial.println("idle");
 #endif
-        prior_state = state;
+        priorState = state;
         motors::brake();
         motors::setSpeedBoth(0U);
     }
 }
 
-void avoidFallFront()
+namespace avoidFallFrontLeft_np
 {
+    const uint_fast8_t reverseDuration = 300U;  // ms
+    const uint_fast8_t maxTurnDuration = 1000U; // ms
     uint_fast32_t t;
 
-    if (state != prior_state)
+    const uint_fast8_t turnAngle = 120; // °
+    uint_fast32_t referenceTime;
+
+    void reverse();
+    void turnRight();
+
+    void reverse()
+    {
+        if (innerState != priorInnerState)
+        {
+#ifdef DEBUG
+            Serial.println("reverse");
+#endif
+            priorInnerState = innerState;
+            referenceTime = millis();
+            motors::setSpeedBoth(5U);
+            motors::goBack();
+        }
+
+        t = millis();
+
+        if (t - referenceTime > reverseDuration)
+        {
+            innerState = turnRight;
+        }
+    }
+
+    void turnRight()
+    {
+        using namespace gyroscope;
+        if (innerState != priorInnerState)
+        {
+#ifdef DEBUG
+            Serial.println("turn right");
+#endif
+            priorInnerState = innerState;
+            mpu.update();
+            motors::setSpeedBoth(5U);
+            motors::turnRight();
+            referenceAngleZ = mpu.getAngleZ();
+            referenceTime = millis();
+        }
+
+        mpu.update();
+        currentAngleZ = mpu.getAngleZ();
+        t = millis();
+
+        if (abs(currentAngleZ - referenceAngleZ) > turnAngle || t - referenceTime > maxTurnDuration)
+        {
+            innerState = NULL;
+        }
+    }
+}
+
+void avoidFallFrontLeft()
+{
+
+    if (state != priorState)
     {
 #ifdef DEBUG
         Serial.println("avoid fall front");
 #endif
-        prior_state = state;
-        avoidFallFrontTime = millis();
-        motors::setSpeedBoth(4U);
-        motors::goBack();
+        priorState = state;
 
-        // if (LINE_FRONT_LEFT_DETECTED)
-        // {
-        //     turnAngle = 90;
-        //     next_state =
-        // }
+        priorInnerState = NULL;
+        innerState = avoidFallFrontLeft_np::reverse;
     }
 
-    t = millis();
-
-    // if (t - avoidFallFrontTime > 500)
-    // {
-    //     if state
-    //         = turn90Right;
-    // }
-}
-
-void attackFront()
-{
-    if (state != prior_state)
+    if (innerState != NULL)
     {
-#ifdef DEBUG
-        Serial.println("attack front");
-#endif
-        prior_state = state;
-        motors::setSpeedBoth(15U);
-        motors::goForward();
-    }
-
-    line::readValues();
-    proximity::readStates();
-
-    if (LINE_FRONT_DETECTED)
-    {
-        state = avoidFallFront;
+        innerState();
         return;
     }
-    
+
+    state = normalSearch;
 }
 
-void normalOperation()
+// void attackFront()
+// {
+//     if (state != priorState)
+//     {
+// #ifdef DEBUG
+//         Serial.println("attack front");
+// #endif
+//         priorState = state;
+//         motors::setSpeedBoth(15U);
+//         motors::goForward();
+//     }
+
+//     line::readValues();
+//     proximity::readStates();
+
+//     if (LINE_FRONT_DETECTED)
+//     {
+//         state = avoidFallFrontLeft;
+//         return;
+//     }
+// }
+
+void normalSearch()
 {
-    if (state != prior_state)
+    if (state != priorState)
     {
 #ifdef DEBUG
         Serial.println("normal operation");
 #endif
-        prior_state = state;
+        priorState = state;
         motors::setSpeedBoth(4U);
         motors::goForward();
     }
 
     line::readValues();
-    if (LINE_FRONT_DETECTED)
+
+    if (LINE_FRONT_LEFT_DETECTED)
     {
-        // state = avoidFallFront;
-        turnAngle = 90;
-        state = turnLeft;
+        state = avoidFallFrontLeft;
         return;
     }
 
-    proximity::readStates();
-    if (OPPONENT_DETECTED_FRONT_CENTER_ONLY){
-
-    }
-    if (OPPONENT_DETECTED_BACK_ONLY)
-    {
-        turnAngle = 180;
-        state = turnLeft;
-        return;
-    }
+    // proximity::readStates();
+    // if (OPPONENT_DETECTED_FRONT_CENTER_ONLY)
+    // {
+    // }
+    // if (OPPONENT_DETECTED_BACK_ONLY)
+    // {
+    //     turnAngle = 180;
+    //     state = turnLeft;
+    //     return;
+    // }
 }
 
-void turnRight()
-{
-    using namespace gyroscope;
-    if (state != prior_state)
-    {
-#ifdef DEBUG
-        Serial.println("turn right");
-#endif
-        prior_state = state;
-        mpu.update();
-        motors::turnRight();
-        motors::setSpeedBoth(4U);
-        referenceAngleZ = mpu.getAngleZ();
-    }
+// void turnRight()
+// {
+//     using namespace gyroscope;
+//     if (state != priorState)
+//     {
+// #ifdef DEBUG
+//         Serial.println("turn right");
+// #endif
+//         priorState = state;
+//         mpu.update();
+//         motors::turnRight();
+//         motors::setSpeedBoth(4U);
+//         referenceAngleZ = mpu.getAngleZ();
+//     }
 
-    mpu.update();
-    currentAngleZ = mpu.getAngleZ();
+//     mpu.update();
+//     currentAngleZ = mpu.getAngleZ();
 
-    if (abs(currentAngleZ - referenceAngleZ) > turnAngle)
-    {
-        state = normalOperation;
-    }
-}
+//     if (abs(currentAngleZ - referenceAngleZ) > turnAngle)
+//     {
+//         state = normalSearch;
+//     }
+// }
 
-void turnLeft()
-{
-    using namespace gyroscope;
-    if (state != prior_state)
-    {
-#ifdef DEBUG
-        Serial.println("turn left");
-#endif
-        prior_state = state;
-        mpu.update();
-        motors::turnLeft();
-        motors::setSpeedBoth(4U);
-        referenceAngleZ = mpu.getAngleZ();
-    }
+// void turnLeft()
+// {
+//     using namespace gyroscope;
+//     if (state != priorState)
+//     {
+// #ifdef DEBUG
+//         Serial.println("turn left");
+// #endif
+//         priorState = state;
+//         mpu.update();
+//         motors::turnLeft();
+//         motors::setSpeedBoth(4U);
+//         referenceAngleZ = mpu.getAngleZ();
+//     }
 
-    mpu.update();
-    currentAngleZ = mpu.getAngleZ();
+//     mpu.update();
+//     currentAngleZ = mpu.getAngleZ();
 
-    if (abs(currentAngleZ - referenceAngleZ) > turnAngle)
-    {
-        state = normalOperation;
-    }
-}
+//     if (abs(currentAngleZ - referenceAngleZ) > turnAngle)
+//     {
+//         state = normalSearch;
+//     }
+// }
+
+// void turn45Left()
+// {
+//     if (innerState != priorInnerState)
+//     {
+//         motors::turnLeft();
+//         motors::setSpeedBoth(5U);
+//         priorInnerState = innerState;
+//     }
+// }
+
+// void diagonalAttack()
+// {
+//     if (state != priorState)
+//     {
+// #ifdef DEBUG
+//         Serial.println("diagonal attack");
+// #endif
+//         priorState = state;
+
+//         priorInnerState = NULL;
+//         innerState = turn45Left;
+//     }
+
+//     innerState();
+
+//     if (LINE_FRONT_DETECTED)
+//     {
+//         state = avoidFallFrontLeft;
+//     }
+// }
 
 #endif
