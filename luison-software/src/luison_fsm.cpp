8846d2f11954effc7786ca24de9b719ee57759b4
Caleb Trepowski <calebtrepowski@gmail.com>
2022-10-10 14:49:23 -0300

    Add avoid fall front rigut routine

--- luison-software/src/luison_fsm.cpp
+++ luison-software/src/luison_fsm.cpp
@@ -16,6 +16,7 @@ typedef void (*INNER_STATE_HANDLER_T)();
 
 void idle();
 void avoidFallFrontLeft();
+void avoidFallFrontRight();
 void attackFront();
 void normalSearch();
 void turnRight();
@@ -201,6 +202,92 @@ void avoidFallFrontLeft()
     state = normalSearch;
 }
 
+namespace avoidFallFrontRight_np
+{
+    const uint_fast8_t reverseDuration = 300U;  // ms
+    const uint_fast8_t maxTurnDuration = 1000U; // ms
+    uint_fast32_t t;
+
+    const uint_fast8_t turnAngle = 120; // °
+    uint_fast32_t referenceTime;
+
+    const uint_fast8_t reverseSpeed = 3U; // ms
+    const uint_fast8_t turnSpeed = 3U;    // ms
+
+    void reverse();
+    void turnLeft();
+
+    void reverse()
+    {
+        if (innerState != priorInnerState)
+        {
+#ifdef DEBUG
+            Serial.println("reverse");
+#endif
+            priorInnerState = innerState;
+            referenceTime = millis();
+            motors::setSpeedBoth(reverseSpeed);
+            motors::goBack();
+        }
+
+        t = millis();
+
+        if (t - referenceTime > reverseDuration)
+        {
+            innerState = turnLeft;
+        }
+    }
+
+    void turnLeft()
+    {
+        using namespace gyroscope;
+        if (innerState != priorInnerState)
+        {
+#ifdef DEBUG
+            Serial.println("turn left");
+#endif
+            priorInnerState = innerState;
+            mpu.update();
+            motors::setSpeedBoth(turnSpeed);
+            motors::turnLeft();
+            referenceAngleZ = mpu.getAngleZ();
+            referenceTime = millis();
+        }
+
+        mpu.update();
+        currentAngleZ = mpu.getAngleZ();
+        t = millis();
+
+        if (abs(currentAngleZ - referenceAngleZ) > turnAngle || t - referenceTime > maxTurnDuration)
+        {
+            innerState = NULL;
+        }
+    }
+}
+
+void avoidFallFrontRight()
+{
+
+    if (state != priorState)
+    {
+#ifdef DEBUG
+        Serial.println("avoid fall front");
+#endif
+        priorState = state;
+
+        priorInnerState = NULL;
+        innerState = avoidFallFrontRight_np::reverse;
+    }
+
+    if (innerState != NULL)
+    {
+        innerState();
+        return;
+    }
+
+    state = normalSearch;
+}
+
 // void attackFront()
 // {
 //     if (state != priorState)
@@ -241,6 +328,12 @@ void normalSearch()
     if (LINE_FRONT_LEFT_DETECTED)
     {
         state = avoidFallFrontLeft;
+        return;
+    }
+    
+    if (LINE_FRONT_RIGHT_DETECTED)
+    {
+        state = avoidFallFrontRight;
         return;
     }
 
