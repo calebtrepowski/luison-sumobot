4f921991fb787d8821a1784cda7433ed1699b637
Caleb Trepowski <calebtrepowski@gmail.com>
2022-12-08 11:48:14 -0300

    Activate bot and test line through bluetooth

--- luison-software/src/luison_fsm.cpp
+++ luison-software/src/luison_fsm.cpp
@@ -12,32 +12,51 @@
 #include "motorZerolag.h"
 #endif
 
-#include "onOffControl.h"
-#include "onOffInterruption.h"
+// #include "onOffControl.h"
+// #include "onOffInterruption.h"
 #include "proximity.h"
 #include "fsm/fsm.h"
+#include "bluetooth.h"
+
+uint_fast8_t active;
 
 void setup()
 {
     gyroscope::setup();
     line::setup();
     motors::setup();
-    onOffControl::setup();
+    // onOffControl::setup();
     proximity::setup();
 
-    onOffInterruption::setOnOffInterruption();
+    bluetooth::setup();
+
+    // onOffInterruption::setOnOffInterruption();
 
     fsm::priorState = NULL;
     fsm::state = fsm::idle;
+
+    active = false;
+
 #ifdef DEBUG
     Serial.begin(9600);
     Serial.println("setup");
+#endif
+
+#ifdef DEBUG_BLUETOOTH
+    bluetooth::SerialBT.println("setup");
 #endif
 }
 
 void loop()
 {
+    if (!active)
+    {
+        fsm::state = fsm::idle;
+    }
     fsm::state();
+    if(bluetooth::loadStart(active)){
+        fsm::state = fsm::normalSearch;
+    };
 }
 
 #endif
--- luison-software/src/tests/test_line.cpp
+++ luison-software/src/tests/test_line.cpp
@@ -1,20 +1,23 @@
 #ifdef TEST_LINE
 #include <Arduino.h>
 #include "line.h"
+#include "bluetooth.h"
 
 void printValues();
+void printValuesBT();
 
 void setup()
 {
     line::setup();
     Serial.begin(9600);
+    bluetooth::setup();
 }
 
 void loop()
 {
     line::readValues();
     Serial.println("-----------");
-    printValues();
+    printValuesBT();
     Serial.println("-----------");
     delay(500);
 }
@@ -24,9 +27,20 @@ void printValues()
     Serial.print(line::frontLeft.value);
     Serial.print("\t");
     Serial.println(line::frontRight.value);
-    Serial.print(line::backLeft.value);
-    Serial.print("\t");
-    Serial.print(line::backRight.value);
+    // Serial.print(line::backLeft.value);
+    // Serial.print("\t");
+    // Serial.print(line::backRight.value);
     Serial.println();
+}
+void printValuesBT()
+{
+    using namespace bluetooth;
+    SerialBT.print(line::frontLeft.value);
+    SerialBT.print("\t");
+    SerialBT.println(line::frontRight.value);
+    SerialBT.print(line::backLeft.value);
+    SerialBT.print("\t");
+    SerialBT.print(line::backRight.value);
+    SerialBT.println();
 }
 #endif
